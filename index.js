41b4e7704df749a7a2d338f9c33b78cb9f4b81a3
katrin <andreevak53@gmail.com>
2018-12-26 08:57:26 +0000

    saved changes

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 let string = "some test string";
-//1 та останній символ
+//1 1 та останній символ
 console.log(string.charAt(0));
 console.log(string.substr(-1));
+console.log(string[string.length-1]);
 
-// перша та остання в верхньому
+//2 перша та остання в верхньому
 console.log(string.charAt(0).toUpperCase() + string.slice(1, -1).toLowerCase() + string.slice(-1).toUpperCase());
+console.log(string[0].toUpperCase());
+console.log(string[string.length - 1].toUpperCase());
 
-// положение слова в строке
+//3 положение слова в строке
 console.log(string.indexOf('string'));
 
-//положение второго пробела
-console.log(string.lastIndexOf(' '));
+//4 положение второго пробела
+// console.log(string.lastIndexOf(' '));
+let secondSpace = string.indexOf(' ', string.indexOf(' ') + 1);
+console.log(secondSpace);
 
-// с 5 символа длиной 4 букви
-console.log(string.substr(5, 4));
+//5 с 5 символа длиной 4 букви
+console.log(string.substr(4, 4));
 
-// с 5 по 9 символи
-console.log(string.slice(5, 9));
+//6 с 5 по 9 символи
+console.log(string.slice(4, 8));
 
-// нова строка без 6 символів останніх
+//7 нова строка без 6 символів останніх
 console.log(string.slice(0, -6));
 
-// //2016 сложение строк
-let a = '20';
-let b = '16';
-let string2 = a + b;
-console.log(string2);
+//8 //2016 сложение строк
+let a = 20;
+let b = 16;
+// let string2 = a + b;
+// console.log(string2);
+console.log(`${a}${b}`);
 
 
-//numbers
-//1.
+//9numbers
+//9. 1.
 console.log(Math.PI.toFixed(2));
 
-//2 max min
+//10. 2 max min
 let i = 15,
     j = 11,
     c = 16,
@@ -44,14 +50,15 @@ let i = 15,
 console.log(Math.min(i, j, c, d, e, f, g, h));
 console.log(Math.max(i, j, c, d, e, f, g, h));
 
-//3
+//11. 3
 let randomNum = Math.random();
-console.log(randomNum.toFixed(2));
-console.log(Math.floor(Math.random() * 20) + 0);
+let fixedNum = randomNum.toFixed(2);
+let floorNum = Math.floor(Math.random() * 20) + 0;
+console.log(parseFloat(floorNum));
 
-//4
+//12. 4
 let x = 0.6 + 0.7;
-console.log(x.toFixed(1));
+console.log(parseFloat(x.toFixed(1)));
 
-//5
-console.log(parseInt('100$'));
+//13. 5
+console.log(parseFloat(parseInt('100$')));
